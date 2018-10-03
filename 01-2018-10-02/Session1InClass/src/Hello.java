87d1a41a1da7f503141dd6b5122a39e367ca4736
aaronwells <chacewells@gmail.com>
2018-10-02 18:23:37 -0700

    hello from aaron

--- 01-2018-10-02/Session1InClass/src/Hello.java
+++ 01-2018-10-02/Session1InClass/src/Hello.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	public static void main(String[] args) throws IOException {
+		Files.newBufferedReader(Paths.get("hello.txt"))
+			.lines()
+			.forEach(System.out::println);
 	}
 }
