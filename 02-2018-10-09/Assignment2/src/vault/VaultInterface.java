7ea0c70694e2a88b344dbdf1873d74441e8ee84f
hanks-su <42451381+hanks-su@users.noreply.github.com>
2018-10-15 20:38:05 -0700

    Modified VaultInterface

--- 02-2018-10-09/Assignment2/src/vault/VaultInterface.java
+++ 02-2018-10-09/Assignment2/src/vault/VaultInterface.java
@@ -2,23 +2,31 @@ package vault;
 
 /*
   In addition the Vault must implement two constructors
+  
 	public Vault();
 	public Vault(encrypt.Encryptor e);
+	
+  The vault must also implement the following method for
+  debugging / testing purposes.  Note that this method
+  has package-level visibility
+  
+  String getEncryptedPassword(String username, String sitename)
+     throws UserNotFountException, SiteNotFoundException;
 */
 
 public interface VaultInterface {
-	public abstract void newUser(String username, String password) throws InvalidUsernameException,
+	public void newUser(String username, String password) throws InvalidUsernameException,
 			InvalidPasswordException, DuplicateUserException;
 
-	public abstract String newSite(String username, String password, String sitename)
+	public String newSite(String username, String password, String sitename)
 			throws vault.DuplicateSiteException, vault.UserNotFoundException,
 			UserLockedOutException, PasswordMismatchException, InvalidSiteException;
 
-	public abstract String updateSite(String username, String password, String sitename)
+	public String updateSite(String username, String password, String sitename)
 			throws SiteNotFoundException, UserNotFoundException,
 			UserLockedOutException, PasswordMismatchException;
 
-	public abstract String retrieveSite(String username, String password, String sitename)
+	public String retrieveSite(String username, String password, String sitename)
 			throws SiteNotFoundException, UserNotFoundException,
 			UserLockedOutException, PasswordMismatchException;
 }
