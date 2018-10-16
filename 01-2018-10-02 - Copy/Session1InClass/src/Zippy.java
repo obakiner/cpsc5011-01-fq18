0c5c0817fee4826854b354e0f9e0992dabad9518
hanks-su <42451381+hanks-su@users.noreply.github.com>
2018-10-15 20:58:43 -0700

    Add encryption project back into lab

--- 01-2018-10-02 - Copy/Session1InClass/src/Zippy.java
+++ 01-2018-10-02 - Copy/Session1InClass/src/Zippy.java
@@ -1,4 +1,25 @@
 
 public class Zippy {
+	private List<String> quoteFile;
+	private static final String FILE_NAME = "C:\\Users\\hanks\\Desktop\\yow.lines";
+
+	public Zippy() {
+		quoteFile = null;
+		try {
+			
+			// Read lines from FILE_NAME into quoteFile
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String toString() {
+		return zippyQuoteFile.get(this.random());
+	}
 
 }
