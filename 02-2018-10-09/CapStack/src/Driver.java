c8f78b5dd4dfb50360d86e24578d39067bd722f9
hanks-su <42451381+hanks-su@users.noreply.github.com>
2018-10-16 14:34:18 -0700

    update capstack driver for demo

--- 02-2018-10-09/CapStack/src/Driver.java
+++ 02-2018-10-09/CapStack/src/Driver.java
@@ -2,11 +2,13 @@ import capstack.CapStackInterface;
 import capstack.CapStack;
 
 public class Driver {
-
 	public static void main(String[] args) {
 		CapStackInterface<String> cs = new CapStack<String>(1);
 		cs.push("hello");
 		cs.push("world");
+		System.out.println(cs.pop());
+		System.out.println(cs.pop());
+		System.out.println(cs.size());
 	}
 
 }
